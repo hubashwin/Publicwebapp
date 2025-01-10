93a8ded24287641d39054b9b9b04a0b83e13db46
hubashwin <ashwinkumaryadav909@gmail.com>
2025-01-10 19:23:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -30,16 +30,6 @@ io.on('connection', (socket) => {
     });
 });
 
-app.get('/get', async (req, res) => {
-    try {
-        const message = await Model.find();
-        res.status(200).json(message)
-    } catch (error) {
-        console.error(error)
-        throw error;
-    }
-})
-
 httpServer.listen(3000, () => {
     console.log('Server started on port 3000');
 });
